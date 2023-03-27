f3f3f63a43db872a37ee30fb924f649196576109
Parker Nilson <parker.todd.nilson@gmail.com>
2023-03-27 12:23:49 -0600

    separate functions, WIP: start creating svelte-functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -19,14 +19,3 @@ export const helloWorld = functions.https.onRequest((request, response) => {
     }
 
 });
-
-let ssrServerServer: any;
-exports.ssrServer = functions.region("us-central1").https.onRequest(async (request, response) => {
-    if (!ssrServerServer) {
-        functions.logger.info("Initialising SvelteKit SSR entry");
-        ssrServerServer = require("./ssrServer/index").default;
-        functions.logger.info("SvelteKit SSR entry initialised!");
-    }
-    functions.logger.info("Requested resource: " + request.originalUrl);
-    return ssrServerServer(request, response);        
-});
